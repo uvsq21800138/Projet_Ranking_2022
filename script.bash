ebdd8943d74c66d402b3b9244293213410659019
Yann <yann.barbier@ens.uvsq.fr>
2022-05-02 16:57:00 +0200

    Programme fonctionnel, premiers test

--- script.bash
+++ script.bash
@@ -17,7 +17,8 @@ mkdir datastudy
 
 ########################################################## Variables
 _start=5
-_staticAlpha=0.5
+_end=96
+_staticAlpha=0.6
 _staticRatio=0.3
 
 ########################################################## Lancement des études
@@ -25,47 +26,139 @@ _staticRatio=0.3
 ### wb-cs-stanford ###
 _graphe=wb-cs-stanford
 _id=1
-_end=100
 _hundred=100
 
 printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
 printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
-for ((c = ${_start}; c < ${_end}; c=c+5))
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
 do
 _ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
-./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.csv 1 0${_ratio} ${_staticAlpha}
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
 progressBar ${c} ${_end}
 done
 
-_end=90
-
 printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
-for ((c = ${_start}; c < ${_end}; c=c+5))
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
 do
 _alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
-./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.csv 1 0${_staticRatio} ${_alpha}
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
 progressBar ${c} ${_end}
 done
 
 ### Stanford ###
 
+_graphe=Stanford
+_id=2
+_hundred=100
 
+printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
+printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
+progressBar ${c} ${_end}
+done
+
+printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
+progressBar ${c} ${_end}
+done
 
 ### Stanford_BerkeleyV2 ###
 
+_graphe=Stanford_BerkeleyV2
+_id=3
+_hundred=100
 
+printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
+printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
+progressBar ${c} ${_end}
+done
+
+printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
+progressBar ${c} ${_end}
+done
 
 ### in-2004v2 ###
 
+_graphe=in-2004v2
+_id=4
+_hundred=100
 
+printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
+printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
+progressBar ${c} ${_end}
+done
 
-### wb-edu.txt ###
+printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
+progressBar ${c} ${_end}
+done
 
+### wb-edu ###
 
+_graphe=wb-edu
+_id=5
+_hundred=100
+
+printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
+printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
+progressBar ${c} ${_end}
+done
+
+printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
+progressBar ${c} ${_end}
+done
 
 ### wikipedia-20051105V2 ###
 
+_graphe=wikipedia-20051105V2
+_id=6
+_hundred=100
 
+printf "/// Etude de %s (%s/6) ///\n" ${_graphe} ${_id}
+printf "Nombre d'itérations avant convergence de chaque algorithme selon le ratio de sommets détruits\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_ratio=$(bc -l <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_ratio.data 1 0${_ratio} ${_staticAlpha}
+progressBar ${c} ${_end}
+done
+
+printf "Nombre d'itérations avant convergence de chaque algorithme selon alpha\n"
+for ((c = ${_start}; c < ${_end}; c=c+${_start}))
+do
+_alpha=$(bc <<< "scale=2; ${c}/${_hundred}")
+./ranking_release data/${_graphe}.txt datastudy/${_graphe}_alpha.data 1 0${_staticRatio} ${_alpha}
+progressBar ${c} ${_end}
+done
 
 ########################################################## Génération des graphiques
 printf "Génération des graphiques en cours...\n"
--- study.R
+++ study.R
@@ -1,9 +1,6 @@
-# Indication du répertoire source
-setwd("/datastudy")
-
 ### wb-cs-stanford ###
 # ratio
-data = read.table("wb-cs-stanford_ratio.csv")
+data = read.table("datastudy/wb-cs-stanford_ratio.data")
 attach(data);
 
 alpha = V1
@@ -12,14 +9,12 @@ nbIterationsCustom = V3
 ratioSommetsDetruits = V4
 ratioArcsDetruits = V5
 
-plot(ratioSommetsDetruits, nbIterationsPagerank, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
-col = "blue", main = "Nombre d'itérations avant convergence selon le ratio de sommets détruits, alpha = 0.3")
-
-points(ratioSommetsDetruits, nbIterationsCustom, col = "red")
-lines(ratioSommetsDetruits, nbIterationsCustom, col = "red")
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
 
 #alpha
-data = read.table("wb-cs-stanford_alpha.csv")
+data = read.table("datastudy/wb-cs-stanford_alpha.data")
 attach(data);
 
 alpha = V1
@@ -28,27 +23,151 @@ nbIterationsCustom = V3
 ratioSommetsDetruits = V4
 ratioArcsDetruits = V5
 
-plot(ratioSommetsDetruits, nbIterationsPagerank, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
-col = "blue", main = "Nombre d'itérations avant convergence selon le ratio de sommets détruits, alpha = 0.3")
-
-points(ratioSommetsDetruits, nbIterationsCustom, col = "red")
-lines(ratioSommetsDetruits, nbIterationsCustom, col = "red")
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
 
 ### Stanford ###
+# ratio
+data = read.table("datastudy/Stanford_ratio.data")
+attach(data);
 
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
 
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
+
+#alpha
+data = read.table("datastudy/Stanford_alpha.data")
+attach(data);
+
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
 
 ### Stanford_BerkeleyV2 ###
+# ratio
+data = read.table("datastudy/Stanford_BerkeleyV2_ratio.data")
+attach(data);
 
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
 
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
+
+#alpha
+data = read.table("datastudy/Stanford_BerkeleyV2_alpha.data")
+attach(data);
+
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
 
 ### in-2004v2 ###
+# ratio
+data = read.table("datastudy/in-2004v2_ratio.data")
+attach(data);
 
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
 
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
 
-### wb-edu.txt ###
+#alpha
+data = read.table("datastudy/in-2004v2_alpha.data")
+attach(data);
 
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
 
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
+
+### wb-edu ###
+# ratio
+data = read.table("datastudy/wb-edu_ratio.data")
+attach(data);
+
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
+
+#alpha
+data = read.table("datastudy/wb-edu_alpha.data")
+attach(data);
+
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
 
 ### wikipedia-20051105V2 ###
+# ratio
+data = read.table("datastudy/wikipedia-20051105V2_ratio.data")
+attach(data);
 
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, ratioSommetsDetruits, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "Ratio de sommets détruits",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, alpha fixé à 0.6")
+lines(nbIterationsCustom, ratioSommetsDetruits, col = "red")
+
+#alpha
+data = read.table("datastudy/wikipedia-20051105V2_alpha.data")
+attach(data);
+
+alpha = V1
+nbIterationsPagerank = V2
+nbIterationsCustom = V3
+ratioSommetsDetruits = V4
+ratioArcsDetruits = V5
+
+plot(nbIterationsPagerank, alpha, type = "l", xlab = "Nombre d'itérations avant convergence", ylab = "alpha",
+col = "blue", main = "Bleu = Pagerank, Rouge = Custom, ratio fixé à 0.3")
+lines(nbIterationsCustom, alpha, col = "red")
