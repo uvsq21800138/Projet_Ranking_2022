36e18824d82afbc43bcb546cd3a13be90fa26ce0
Yann <yann.barbier@ens.uvsq.fr>
2022-05-14 23:57:41 +0200

    Valeurs des variables du script et R

--- make_study.sh
+++ make_study.sh
@@ -3,12 +3,12 @@
 # Constants
 input_dir=data
 output_dir=datastudy
-alpha_min=0.8
+alpha_min=0.85
 alpha_max=0.95
 alpha_static=0.8
-r_min=0.01
-r_max=0.5
-r_static=0.2
+r_min=0.05
+r_max=0.15
+r_static=0.05
 
 function run_study
 {
@@ -22,7 +22,7 @@ function run_batch_alpha
 	n=$2
 	alpha_step=$3
 	for name in $1; do
-		run_study $name "${name}_alpha" $n $alpha_min $alpha_max $alpha_step $r_static $r_static $r_static 1
+		run_study $name "${name}_alpha" $n $alpha_min $alpha_max $alpha_step $r_min $r_max 0.05
 	done
 }
 
@@ -31,7 +31,7 @@ function run_batch_ratio
 	n=$2
 	r_step=$3
 	for name in $1; do
-		run_study $name "${name}_ratio" $n $alpha_static $alpha_static 1 $r_min $r_max $r_step
+		run_study $name "${name}_ratio" $n $alpha_min $alpha_max 0.05 $r_min $r_max $r_step
 	done
 }
 
@@ -48,16 +48,16 @@ mkdir -p datastudy
 run_batch_alpha 'wb-cs-stanford' 100 0.01
 run_batch_ratio 'wb-cs-stanford' 10 0.01
 # <= 50M
-run_batch_alpha 'Stanford' 100 0.02
-run_batch_ratio 'Stanford' 10 0.02
+run_batch_alpha 'Stanford' 100 0.01
+run_batch_ratio 'Stanford' 10 0.01
 # <= 150M
-run_batch_alpha 'Stanford_BerkeleyV2' 100 0.02
-run_batch_ratio 'Stanford_BerkeleyV2' 10 0.02
+run_batch_alpha 'Stanford_BerkeleyV2' 100 0.01
+run_batch_ratio 'Stanford_BerkeleyV2' 10 0.01
 # <= 500M
-run_batch_alpha 'in-2004v2 wikipedia-20051105V2' 50 0.1
-run_batch_ratio 'in-2004v2 wikipedia-20051105V2' 10 0.05
+run_batch_alpha 'in-2004v2 wikipedia-20051105V2' 50 0.01
+run_batch_ratio 'in-2004v2 wikipedia-20051105V2' 10 0.01
 # > 500M
-run_batch_alpha 'wb-edu' 50 0.5
-run_batch_ratio 'wb-edu' 10 0.5
+run_batch_alpha 'wb-edu' 50 0.01
+run_batch_ratio 'wb-edu' 10 0.01
 
 R CMD BATCH study.R
--- study.R
+++ study.R
@@ -1,89 +1,75 @@
+# Variables
 nbGraphes <- 6
 
+# Fonctions
 plots_for_one_graph <- function(name, id){
     # Ratio de sommets détruits
     data = read.table(paste0("datastudy/", name, "_ratio.data"))
     attach(data)
 
     alpha = V1
-    nbIterationsPagerank = V2
-    nbIterationsCustom = V3
-    ratioSommetsDetruits = V4
-    ratioArcsDetruits = V5
+    acceleration = V2
+    ratioSommetsDetruits = V3
+    ratioArcsDetruits = V4
 
-    lowY = min(nbIterationsPagerank, nbIterationsCustom)
-    highY = max(nbIterationsPagerank, nbIterationsCustom)
+    data[data$alpha == 0.80,]
     
-    plot(ratioSommetsDetruits, nbIterationsPagerank, 
-         pch = 20, 
-         cex = 0.5, 
-         xlim=c(0, 0.5), 
-         ylim=c(lowY, highY),
-         xlab = "Ratio de sommets détruits", 
-         ylab = "Nombre d'itérations avant convergence", 
+    plot(ratioSommetsDetruits, acceleration, 
+         pch = 20,
+         cex = 0.5,
+         xlab = "Ratio de sommets détruits",
+         ylab = "Accélération de l'algorithme custom (%)",
          col = "blue",
-         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nBleu = Pagerank, Rouge = Custom, Alpha fixé à 0.8")
-    )
-    lines(lowess(ratioSommetsDetruits, nbIterationsPagerank, f = 0.3), 
-          col = "blue")
-    points(ratioSommetsDetruits, nbIterationsCustom, 
-           pch = 20, 
-           cex = 0.5, 
-           col = "red"
-    )
-    lines(lowess(ratioSommetsDetruits, nbIterationsCustom, f = 0.3), 
-          col = "red")
+         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nAlpha fixé à : bleu = 0.80, vert = 0.85, rouge = 0.90"))
+    lines(lowess(ratioSommetsDetruits, acceleration, f = 0.3), col = "blue")
+    data[data$alpha == 0.85,]
+    points(ratioSommetsDetruits, acceleration, pch = 20, cex = 0.5, col = "green")
+    lines(lowess(ratioSommetsDetruits, acceleration, f = 0.3), col = "green")
+    data[data$alpha == 0.90,]
+    points(ratioSommetsDetruits, acceleration, pch = 20, cex = 0.5, col = "red")
+    lines(lowess(ratioSommetsDetruits, acceleration, f = 0.3), col = "red")
 
     # Ratio d'arcs détruits
     plot(ratioSommetsDetruits, ratioArcsDetruits, 
-         pch = 20, 
-         cex = 0.5, 
-         xlab = "Ratio de sommets détruits", 
-         ylab = "Ratio d'arcs détruits", 
+         pch = 20,
+         cex = 0.5,
+         xlab = "Ratio de sommets détruits",
+         ylab = "Ratio d'arcs détruits",
          col = "blue",
-         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nAlpha fixé à 0.8")
-    )
-    lines(lowess(ratioSommetsDetruits, ratioArcsDetruits), 
-          col = "blue")
+         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nAlpha fixé à : bleu = 0.80, vert = 0.85, rouge = 0.90"))
+    lines(lowess(ratioSommetsDetruits, ratioArcsDetruits), col = "blue")
+    points(ratioSommetsDetruits, ratioArcsDetruits, pch = 20, cex = 0.5, col = "green")
+    lines(lowess(ratioSommetsDetruits, ratioArcsDetruits, f = 0.3), col = "green")
+    points(ratioSommetsDetruits, ratioArcsDetruits, pch = 20, cex = 0.5, col = "red")
+    lines(lowess(ratioSommetsDetruits, ratioArcsDetruits, f = 0.3), col = "red")
 
     # Alpha
     data = read.table(paste0("datastudy/", name, "_alpha.data"))
     attach(data)
 
     alpha = V1
-    nbIterationsPagerank = V2
-    nbIterationsCustom = V3
-    ratioSommetsDetruits = V4
-    ratioArcsDetruits = V5
-
-    lowY = min(nbIterationsPagerank, nbIterationsCustom)
-    highY = max(nbIterationsPagerank, nbIterationsCustom)
+    acceleration = V2
+    ratioSommetsDetruits = V3
+    ratioArcsDetruits = V4
 
     plot(alpha, nbIterationsPagerank, 
-         pch = 20, 
-         cex = 0.5, 
-         xlim=c(0.5, 1), 
-         ylim=c(lowY, highY),
-         xlab = "Alpha", 
-         ylab = "Nombre d'itérations avant convergence", 
+         pch = 20,
+         cex = 0.5,
+         xlab = "Alpha",
+         ylab = "Accélération de l'algorithme custom (%)",
          col = "blue",
-         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nBleu = Pagerank, Rouge = Custom, Ratio fixé à 0.2")
-    )
-    lines(lowess(alpha, nbIterationsPagerank, f = 0.3), 
-          col = "blue")
-    points(alpha, nbIterationsCustom, 
-           pch = 20, 
-           cex = 0.5, 
-           col = "red"
-    )
-    lines(lowess(alpha, nbIterationsCustom, f = 0.3), 
-          col = "red")
+         main = paste0(name, " (", id, "/", nbGraphes, ")", "\nRatio fixé à : bleu = 0.05, vert = 0.10, rouge = 0.15"))
+    lines(lowess(alpha, nbIterationsPagerank, f = 0.3), col = "blue")
+    points(alpha, nbIterationsCustom, pch = 20, cex = 0.5, col = "green")
+    lines(lowess(alpha, nbIterationsCustom, f = 0.3), col = "green")
+    points(alpha, nbIterationsCustom, pch = 20, cex = 0.5, col = "red")
+    lines(lowess(alpha, nbIterationsCustom, f = 0.3), col = "red")
 }
 
+# Main
 plots_for_one_graph("wb-cs-stanford", 1)
 plots_for_one_graph("Stanford", 2)
 plots_for_one_graph("Stanford_BerkeleyV2", 3)
 plots_for_one_graph("in-2004v2", 4)
 plots_for_one_graph("wb-edu", 5)
 plots_for_one_graph("wikipedia-20051105V2", 6)
-
